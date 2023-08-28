f5673b4de45b694c62c6d228f30cb2295be3ff31
farzane-maleki <farzane.maleki.fm@gmail.com>
2023-08-28 18:01:12 +0430

    Add Product Controller but in sync way with Database

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext dbContext;
+
+        public ProductsController(StoreContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts() 
+        {
+            var products = dbContext.Products.ToList();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id) 
+        {
+            var product = dbContext.Products.SingleOrDefault(p => p.Id == id);
+            return Ok(product);
+        }
+    }
+}
