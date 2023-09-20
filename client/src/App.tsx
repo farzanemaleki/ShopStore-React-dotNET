8dd5b04090c075620b017e51582c84a45cf39278
farzane-maleki <farzane.maleki.fm@gmail.com>
2023-09-20 14:59:45 +0430

    Add interface for Product and use jsontots website for more convenice

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Product } from './Product';
 function App() {
-  const[products, setProducts] = useState([
-    {name:"p1", price:100},
-    {name:"p2", price:200}
-  ]);
+  const[products, setProducts] = useState<Product[]>([]);
   useEffect(() =>{
     fetch("https://localhost:7289/api/Products")
     .then(response => response.json())
     .then(data => setProducts(data))
   },[]);
   const AddProduct = () =>{
-    setProducts(prevState => [...prevState, {name: `p${prevState.length+1}`, price: (prevState.length+1)*100 }]);
+    setProducts(prevState => [...prevState, {
+      id: prevState.length + 101,
+      name: `p${prevState.length+1}`, 
+      description: "some desscription",
+      pictureUlr: "some image URL",
+      price: (prevState.length+1)*100,
+      brand: "new brand",
+    }]);
   }
   return (
     <div>
       <h1>Re-Store</h1>
       <ul>
-        {products.map((p,index) => (
-          <li key={index}>{p.name} with price ${p.price}</li>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} with price ${product.price}</li>
         ))}
       </ul>
       <button onClick={AddProduct}>Add Product</button>
