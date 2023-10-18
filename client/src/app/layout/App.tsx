8c4a22255c904d6bc05d5263712dbe8d4dbb5369
farzane-maleki <farzane.maleki.fm@gmail.com>
2023-10-18 13:28:55 +0330

    Add Dark mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,17 +1,35 @@
-import './styles.css';
 import Catalog from '../../features/catalog/Catalog';
 import Header from './Header';
-import { Container, CssBaseline } from '@mui/material';
+import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+
+import './styles.css';
+import { useState } from 'react';
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const themeType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: themeType,
+      background: {
+        default: themeType === "light" ? "#eaeaea" : "#140b0bd6"
+      }
+    }
+  })
+  function ToggleTheme() {
+    setDarkMode(!darkMode);
+  }
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header onChange={ToggleTheme} darkMode={darkMode}/>
       <Container>
         <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,12 +1,22 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, IconButton, Theme, Toolbar, Typography } from "@mui/material";
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
-export default function Header() {
+interface HeaderProps {
+    onChange: () => void;
+    darkMode: Boolean;
+  }
+
+export default function Header(props: HeaderProps) {
     return(
     <AppBar position="static" sx={{ mb: 4 }}>
         <Toolbar>
             <Typography variant="h5">
                 Store
             </Typography>
+            <IconButton sx={{ ml: 1 }} onClick={props.onChange} color="inherit">
+                {props.darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
         </Toolbar>
     </AppBar>
     );
